2ab9018e2ec030d3dd13b90cf8e555b62ea5d552
Noonexyz1 <aries-d2github@hotmail.com>
2024-08-06 19:23:30 -0400

    feat: login: Formulario Reactivo Login agregado

--- login/src/app/auth/login/login.component.ts
+++ login/src/app/auth/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms'; //Esto ya esta injectado en Angular
+import { accountModelLogin } from './model/AccountModel';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  private formBuilder: FormBuilder;
+  
+  //Estamos traendo la dependencia que ya esta injectado en Angular
+  constructor(formBuilder: FormBuilder){
+    this.formBuilder = formBuilder;
+  }
+  
+  getLoginFormGroup(): FormGroup | any {
+    return this.formBuilder.group(accountModelLogin);
+  }
 }
---
+++ login/src/app/auth/login/model/AccountModel.ts
@@ -0,0 +1,4 @@
+export const accountModelLogin = {
+    emailAtribute: ['accountModelLogin@valid.com'],
+    passwordAtribute: [''],
+}
