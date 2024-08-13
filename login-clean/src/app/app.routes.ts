d6730e416682162869f33ac41b50363c5f524164
Noonexyz1 <aries-d2github@hotmail.com>
2024-08-13 14:44:42 -0400

    feat: login-clean: Rutas implementado

--- login-clean/src/app/app.routes.ts
+++ login-clean/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './infrastructure/ui/components/dashboard/dashboard.component';
+import { LoginComponent } from './infrastructure/ui/components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/inicio', pathMatch: 'full'},
+    {path: 'inicio', component: DashboardComponent},
+    {path: 'iniciar-sesion', component: LoginComponent},
+];
--- login-clean/src/app/infrastructure/ui/layout/layout.component.ts
+++ login-clean/src/app/infrastructure/ui/layout/layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from '../components/login/login.component';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
